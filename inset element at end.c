8eb48a84513cbfd2a983549dc8da1b04c5c1efb5
mounika192111727 <113505434+mounika192111727@users.noreply.github.com>
2022-09-14 01:34:54 -0700

    Add files via upload

---
+++ inset element at end.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+void main()
+{
+    int position, i, n, value,ch, arr[100];
+    printf("C Program to insert element at end of Array\n");
+    printf("First enter number of elements you want in Array\n");
+    scanf("%d", &n);
+    arr[n];
+   for(i = 0; i < n; i++)
+    {
+        printf("Please give value for index %d : ",i);
+        scanf("%d",&arr[i]);
+    }
+    printf("Let's Insert Element at end \n ");
+    printf("Please give a number to insert at end \n");
+    scanf("%d", &value);
+    arr[n] = value;
+    printf("Element %d is inserted at %d index \n",value,n);
+    printf("New Array is \n ");
+     
+    for(i = 0; i < n+1; i++)
+    {
+       printf("%d \t",arr[i]);
+    }
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+You have no unread notifications
