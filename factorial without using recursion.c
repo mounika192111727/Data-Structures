4872e32216af7b49843d330bad1e4cd058c41354
mounika192111727 <113505434+mounika192111727@users.noreply.github.com>
2022-09-15 00:23:52 -0700

    Add files via upload

---
+++ factorial without using recursion.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main()
+{
+    int n, i;
+    unsigned long long factorial = 1;
+ 
+    printf("Enter a number to find factorial: ");
+    scanf("%d",&n);
+ 
+    if (n < 0)
+        printf("Error! Please enter any positive integer number");
+ 
+    else
+    {
+        for(i=1; i<=n; ++i)
+        {
+            factorial *= i;              
+        }
+        printf("Factorial of Number %d = %llu", n, factorial);
+    }
+ 
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
